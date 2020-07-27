df3c3facdce76c0fff207cc2249e53864494ed5c
xiao_e_yun <67814228+QAZXSW517@users.noreply.github.com>
2020-07-27 18:20:28 +0800

    owo

---
+++ docs/javascript/page.js
@@ -0,0 +1,9 @@
+function steam_page() { //steam¦h±b¤á­¶­±
+    var display;
+    var display = document.getElementById("steam_page").style.display;
+ if(display == "block"){
+     document.getElementById("steam_page").style.display = "none";
+ }else{
+     document.getElementById("steam_page").style.display = "block";
+    }
+}
