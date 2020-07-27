74254c214dfe230b1bafd0400264197faea3fdb0
xiao_e_yun <67814228+QAZXSW517@users.noreply.github.com>
2020-07-27 21:41:16 +0800

    QAQ完成

--- docs/javascript/page.js
+++ docs/javascript/page.js
@@ -1,9 +1,20 @@
-function steam_page() { //steam多帳戶頁面
-    var display;
-    var display = document.getElementById("steam_page").style.display;
- if(display == "block"){
-     document.getElementById("steam_page").style.display = "none";
+function steam_page() { //steam頁面
+    var steam_display = document.getElementById("steam_page").style.display
+ if(steam_display == "block"){
+     document.getElementById("steam_page").style.opacity = "0";
+     setTimeout(steam0,300)     
  }else{
-     document.getElementById("steam_page").style.display = "block";
-    }
+document.getElementById("steam_page").style.display = "block";
+     setTimeout(steam1, 5)
+ }
+}
+function steam1() {//steam開啟
+    document.getElementById("steam_page").style.opacity = "1";
+}
+function steam0() {//steame關閉
+    document.getElementById("steam_page").style.display = "none";
+}
+
+function twitter_page() {//
+
 }
