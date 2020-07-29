c699a6143deafee5ad413ece2cba27df25420519
xiao_e_yun <67814228+QAZXSW517@users.noreply.github.com>
2020-07-29 23:06:18 +0800

    修改文檔和排序=A=

--- docs/javascript/page.js
+++ docs/javascript/page.js
@@ -1,18 +1,14 @@
-function copy_link(link) {
-    console.log(link)
-    var copy = document.getElementById(link)
-    var tag = link + "_tag"
-    console.log(copy)
-    window.getSelection().selectAllChildren(copy);
-    document.execCommand("copy")
-    console.log("discord copy")
-    document.getElementById(tag).style.display = "block"
-    document.getElementById(link).style.opacity = "0"
-    document.getElementById(link).disabled =true
-    setTimeout(copy_back,1500,tag,link)
+window.onload =hello();//啟動加載
+function hello() {
+    console.log("中文")
+    setTimeout(hello_time);
 }
-function copy_back(tag,link) {
-    document.getElementById(tag).style.display = "none"
-    document.getElementById(link).style.opacity = "1"
-    document.getElementById(link).disabled =false
+function time() {
+    var d = new Date();
+    console.log(d.getHours())
+}
+function hello_time() {
+    var d = new Date();
+    console.log("現在時間"+d.getHours())
+
 }
--- docs/javascript/web_link.js
+++ docs/javascript/web_link.js
@@ -9,7 +9,7 @@ function clean_page() {//關閉所有頁面
         }
     }
 }
-function web(app) { //steam
+function web(app) { //執行
     var display = document.getElementById(app + "_page").style.display;
     console.log(app + "_before_" + display)
     setTimeout(clean_page)
@@ -17,16 +17,34 @@ function web(app) { //steam
         setTimeout(web_page, 0, app)
     }
 }
-function web_page(page) { //steam頁面
+function web_page(page) { //啟動頁面
     document.getElementById(page+"_page").style.display = "block";
     setTimeout(web_open, 100,page)
     console.log("open_"+page)
 }
-function web_open(page) {//steam開啟
+function web_open(page) {//顯示
     document.getElementById(page+"_page").style.opacity = "1";
     console.log(page+"page_on")
 }
-function web_clean(page) {//steame關閉
+function web_clean(page) {//隱藏
     document.getElementById(page+"_page").style.display = "none";
     console.log(page+"_page_off")
+}
+function copy_link(link) {//discord複製鍵
+    console.log(link)
+    var copy = document.getElementById(link)
+    var tag = link + "_tag"
+    console.log(copy)
+    window.getSelection().selectAllChildren(copy);
+    document.execCommand("copy")
+    console.log("discord copy")
+    document.getElementById(tag).style.display = "block"
+    document.getElementById(link).style.opacity = "0"
+    document.getElementById(link).disabled = true
+    setTimeout(copy_back, 1500, tag, link)
+}
+function copy_back(tag, link) {//discord複製鍵復原
+    document.getElementById(tag).style.display = "none"
+    document.getElementById(link).style.opacity = "1"
+    document.getElementById(link).disabled = false
 }
