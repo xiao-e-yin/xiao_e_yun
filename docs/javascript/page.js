dd2b3d628b3c2205bcce0216c1f1d156d8894daa
xiao_e_yun <67814228+QAZXSW517@users.noreply.github.com>
2020-07-30 08:20:23 +0800

    準備cookie

--- docs/javascript/page.js
+++ docs/javascript/page.js
@@ -6,9 +6,35 @@ function hello() {
 function time() {
     var d = new Date();
     console.log(d.getHours())
+    setInterval(time, 100000)
 }
 function hello_time() {
-    var d = new Date();
-    console.log("現在時間"+d.getHours())
-
+    var cookie = document.cookie;
+    var day = new Date();
+    var time = day.getHours();
+    console.log(cookie + "現在時間" + time + "點")
+    var user_name = cookie[0]
+    if (user_name = "undefined") {
+        user_name ="陌生人"
+    }
+    if (time >= 21) {
+        document.getElementById("hello_hours").innerHTML = user_name + " 快點睡了"
+        console.log("晚上")
+    }
+    if (time <= 5) {
+        document.getElementById("hello_hours").innerHTML = user_name + " 還沒睡麻"
+        console.log(cookie + "凌晨")
+    }
+    if (time >= 11) {
+        if (time <= 20) {
+            document.getElementById("hello_hours").innerHTML = user_name + " 午安阿"
+            console.log(cookie + "中午")
+        }
+    }
+    if (time >= 6) {
+        if (time <= 10) {
+            document.getElementById("hello_hours").innerHTML = user_name + " 早上好阿"
+            console.log(cookie + "早上")
+        }
+    }
 }
