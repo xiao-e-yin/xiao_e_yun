91001e3a4db61a6b112d1fe3b8aa5400717db202
Xiao_e_yun <67814228+xiao-e-yun@users.noreply.github.com>
2020-09-12 22:44:28 +0800

    Update page.js

--- docs/javascript/page.js
+++ docs/javascript/page.js
@@ -9,7 +9,7 @@ function mode() {
         "background-image": "url('https://raw.githubusercontent.com/xiao-e-yun/home/master/docs/images/background.png')"
     })
     setTimeout(function () {
-        document.getElementsByClassName('background').addEventListener("load",function() {
+        window.addEventListener("load",function() {
             $(".background").css({
                 "opacity": "1"
             })
